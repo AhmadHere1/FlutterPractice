fe84bf544e19ff059574810fbe3a69e1c7053e42
AhmadHere <aghazi1011@gmail.com>
2022-08-03 01:41:05 -0700

    completed

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@
 
 import 'package:first_project/screens/home_page.dart';
 import 'package:first_project/screens/login_page.dart';
+import 'package:first_project/utils/routes.dart';
 import 'package:flutter/material.dart';
 
 // import 'screens/login_page.dart';
@@ -16,19 +17,19 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-     
       themeMode: ThemeMode.light,
       theme: ThemeData(
         primarySwatch: Colors.deepPurple,
-      )
-      ,
+      ),
+      debugShowCheckedModeBanner: true,
       darkTheme: ThemeData(
         brightness: Brightness.dark,
         primaryColor: Colors.black,
       ),
       routes: {
-        "/":(context) => LoginPage(),
-        "/home":(context) => HomePage(),
+        "/": (context) => LoginPage(),
+        MyRoutes.loginRoute: (context) => LoginPage(),
+        MyRoutes.homeRoute: (context) => HomePage(),
       },
     );
   }
--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -1,3 +1,4 @@
+import 'package:first_project/utils/routes.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
@@ -5,56 +6,60 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Material(
         color: Colors.white,
-        child: Column(
-          children: [
-            SizedBox(
-              height: 20,
-            ),
-            Image.asset(
-              'assets/images/login_page.png',
-              fit: BoxFit.cover,
-            ),
-            SizedBox(
-              height: 12,
-            ),
-            Text(
-              'Welcome to Login Page',
-              style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 20,
               ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(children: [
-                TextFormField(
-                  decoration: InputDecoration(
-                    labelText: 'Email',
-                    hintText: 'Enter your email',
+              Image.asset(
+                'assets/images/login_page.png',
+                fit: BoxFit.cover,
+              ),
+              SizedBox(
+                height: 12,
+              ),
+              Text(
+                'Welcome to Login Page',
+                style: TextStyle(
+                  fontSize: 26,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'Email',
+                      hintText: 'Enter your email',
+                    ),
                   ),
-                ),
-                TextFormField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    labelText: 'Password',
-                    hintText: 'Enter your Password',
+                  TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      labelText: 'Password',
+                      hintText: 'Enter your Password',
+                    ),
                   ),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                ElevatedButton(
-                  child: Text('Login'),
-                  onPressed: () {
-                    print('Login Button Pressed');
-                  },
-                ),
-              ]),
-            )
-          ],
+                  SizedBox(
+                    height: 20,
+                  ),
+                  ElevatedButton(
+                    child: Text('Login'),
+                    style: TextButton.styleFrom(minimumSize: Size(300, 40)),
+                    onPressed: () {
+                      Navigator.pushNamed(context, MyRoutes.homeRoute);
+                    },
+                  ),
+                ]),
+              )
+            ],
+          ),
         ));
   }
 }
